b15cd541bebf8697e5a99b6e8e8ae299bef5571e
MichaelJrTheUltimate <mwnoggins24@coe.edu>
2025-01-29 17:35:46 +0000

    Save

---
+++ string_1.py
@@ -0,0 +1,15 @@
+def WordAmount():
+    myStr= "This is fun, really really fun.  I'm old."
+    numSpace = myStr.split(" ")
+    print(len(numSpace))
+    return WordAmount
+
+def ExtraSpaces(WordAmount):
+    WordAmount.find("  ")
+    return ExtraSpaces
+
+def TrueAmount(WordAmount, ExtraSpaces):
+    WordAmount - ExtraSpaces
+    print(TrueAmount)
+
+
